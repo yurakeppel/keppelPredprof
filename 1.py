f6387343730b022d1af677b920b310ad1a0ac318
yurakeppel <158739943+yurakeppel@users.noreply.github.com>
2024-02-03 17:12:37 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,11 +1,16 @@
 import csv
 
+# Открываем файл
 with open("game.txt",encoding="utf8") as file:
     file = file.readlines()
+
+    # Создаем отдельные списки для столбцов
     GameName=[]
     characters=[]
     nameError=[]
     date=[]
+
+    # Разделяем строки на списки
     for i in range(len(file)):
         for j in range(len(file[i])):
             if file[i][j]=="$":
@@ -27,11 +32,14 @@ with open("game.txt",encoding="utf8") as file:
     for i in range(len(file)):
         date.append(file[i][:-1])
 
+    # Ищем ошибки с номером 55, после чего заеняем номер ошибки на "Done", а дату на "0000-00-00"
     for i in range(len(nameError)):
         if "55" in nameError[i]:
             print(f"У персонажа {characters[i]} в игре {GameName[i]} нашлась ошибка с кодом: {nameError[i]} Дата фиксации: {date[i]}")
             nameError[i]="Done"
+            date[i]="0000-00-00"
 
+# Записываем данные в новый файл "game_new.csv"
 with open("game_new.csv","w",encoding="utf8") as f:
     f = csv.writer(f,lineterminator="\n")
     for i in range(len(file)):
