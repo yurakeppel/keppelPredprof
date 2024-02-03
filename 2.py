9cee371e9811ff57396618d42bee9b9066453c5d
yurakeppel <158739943+yurakeppel@users.noreply.github.com>
2024-02-03 17:15:22 +0300

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,8 +1,11 @@
-
+# Открываем файл
 with open("game.txt",encoding="utf8") as file:
     file = file.readlines()
+
+    # Создаем отдельные списки для столбцов
     GameNames=[]
 
+    # Разделяем строки на списки
     for i in range(len(file)):
         for j in range(len(file[i])):
             if file[i][j]=="$":
@@ -10,10 +13,12 @@ with open("game.txt",encoding="utf8") as file:
                 file[i]=file[i][j+1:]
                 break
 
+    # Сортировка списка игр по алфавиту
     GameNames=sorted(GameNames)
     count=1
-
+    # Вывод количества ошибок в играх
     for i in range(len(GameNames)-1):
+        # Убираем повторы игр
         if GameNames[i]==GameNames[i+1]:
             count+=1
         else:
